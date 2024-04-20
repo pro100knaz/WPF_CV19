255256b79c5bd7e125cc7fafd09f873e7418f96b
MFlEGENDANTON <jffgx@mail.ru>
2024-04-20 21:08:07 +0300

    Добавляем пакет для визуализвции данных

--- CV19Main/ViewModels/MainWindowViewModel.cs
+++ CV19Main/ViewModels/MainWindowViewModel.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using CV19Main.Infrastructure.Commands;
+using CV19Main.Models;
 using CV19Main.ViewModels.Base;
 
 namespace CV19Main.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints
 
+        private IEnumerable<DataPoint> _testDataPoints;
+        /// <summary>
+        /// Тестовый набор данных для визуализации графиков
+        /// </summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _testDataPoints;
+            set => SetField(ref _testDataPoints, value);
+        }
 
-        #region Команды
+        #endregion
 
-        #region CloseApplicationCommand
+        #region Commands 
+
+        #region CloseApplicationCommand   
 
 
         public ICommand CloseApplicationCommand { get; }
@@ -35,12 +48,26 @@ namespace CV19Main.ViewModels
         public MainWindowViewModel()
         {
 
+
+
             #region Commands
 
             //CloseApplicationCommand =
             //    new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+
+            var data_points = new List<DataPoint>((int) (360/0.1));
+
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_points.Add(new DataPoint() {XValue = x, YValue = y});
+            }
+
+            TestDataPoints = data_points;
         }
 
 
@@ -69,7 +96,7 @@ namespace CV19Main.ViewModels
 
             private string _status = "Ready";
 
-        /// <summary>
+            /// <summary>
         /// Поле описывающее состояние программмы
         /// </summary>
             public string Status
