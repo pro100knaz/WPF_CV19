7d765e0c4ee359332451dec68d0eefe0e772995c
MFlEGENDANTON <jffgx@mail.ru>
2024-04-20 20:00:19 +0300

    запрос к удалённому сервису

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+
+const string data_uri = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+
+var client = new HttpClient();
+
+var response = client.GetAsync(data_uri).Result;
+
+var csv_str = response.Content.ReadAsStringAsync().Result;
+
+Console.Write("jesfsfef");
+Console.ReadLine();
