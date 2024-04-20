23c5c1797a937c100a70d461fda8035edb306782
MFlEGENDANTON <jffgx@mail.ru>
2024-04-20 20:23:48 +0300

    Извлечение данных по датам

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,14 +1,75 @@
 ﻿using System;
+using System.Globalization;
 using System.Net;
+using System.Linq;
 
-const string data_uri = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
 
 var client = new HttpClient();
 
-var response = client.GetAsync(data_uri).Result;
+//var response = client.GetAsync(data_uri).Result;
 
-var csv_str = response.Content.ReadAsStringAsync().Result;
+//var csv_str = response.Content.ReadAsStringAsync().Result;
+
+
+
+
+//foreach (var dataLine in GetDataLines())
+//{
+//    Console.WriteLine(dataLine);
+//}
+
+var dates = GetDates();
+//foreach (var data in dates)
+//{
+//            Console.WriteLine(data.ToString());
+//}
+
+Console.WriteLine(string.Join("\r\n", dates));
+
+
+
+
+///
+///Поток данных
+/// 
+static async Task<Stream> GetDataStream()
+{
+    var client = new HttpClient();
+
+    var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+
+    return await response.Content.ReadAsStreamAsync();
+}
+
+///Генератор строк
+/// разбивает поток на строки
+
+static  IEnumerable<string> GetDataLines()
+{
+    using var data_stream =  GetDataStream().Result;
+
+    using var data_reader = new StreamReader(data_stream);
+
+    while (!data_reader.EndOfStream)
+    {
+        var line = data_reader.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(line)) continue;
+        yield return line;
+
+    }
+}
+
+static DateTime[] GetDates() => GetDataLines()
+    .First()
+    .Split(',')
+    .Skip(4)
+    .Select(s=> DateTime.Parse(s, CultureInfo.InvariantCulture))
+    .ToArray();
+
+
 
 Console.Write("jesfsfef");
 Console.ReadLine();
