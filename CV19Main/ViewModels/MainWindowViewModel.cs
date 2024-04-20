bf70c62b112d8debad97c54bd1246463d0d9481d
MFlEGENDANTON <jffgx@mail.ru>
2024-04-20 16:47:20 +0300

    базовая view model + первичная разметка окна

---
+++ CV19Main/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19Main.ViewModels.Base;
+
+namespace CV19Main.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title;
+
+        /// <summary> Заголовок окна </summary>
+        public string Title {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                //if (Equals(value, _title)) return;
+                //_title = value;
+                
+                //OnPropertyChanged();
+                
+                SetField(ref _title, value);
+            }
+        }
+    }
+}
