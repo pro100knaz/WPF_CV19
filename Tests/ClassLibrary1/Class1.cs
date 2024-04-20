118e0f87d34a57a6976763cb5bcc49635e115819
MFlEGENDANTON <jffgx@mail.ru>
2024-04-20 18:27:46 +0300

    Добавлен главный класс команды

---
+++ Tests/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using System;
+
+
+namespace ClassLibrary1
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object? parameter);
+
+        public abstract void Execute(object? parameter);
+
+    }
+}
