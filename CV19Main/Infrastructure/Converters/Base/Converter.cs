6121066efca6db639b806796c930d36a57f5ec98
MFlEGENDANTON <jffgx@mail.ru>
2024-04-28 12:05:14 +0300

    Конверторы , ресурсы кистей, блокировка Freezable

---
+++ CV19Main/Infrastructure/Converters/Base/Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CV19Main.Infrastructure.Converters.Base
+{
+    abstract class Converter :IValueConverter
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
+    }
+}
---
+++ CV19Main/Infrastructure/Converters/LinearConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using CV19Main.Infrastructure.Converters.Base;
+
+namespace CV19Main.Infrastructure.Converters
+{
+    /// <summary>
+    /// Реализация линейного преобразования f(x) = k*x + b
+    /// </summary>
+    class LinearConverter : Converter
+    {
+        public double K { get; set; } = 1;
+        public double B { get; set; } = 0;
+        public LinearConverter() { }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            var x = System.Convert.ToDouble(value , culture);
+            return K *x + B;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+
+            var y = System.Convert.ToDouble(value, culture);
+
+            return (y - B) / K;
+        }
+    }
+}
