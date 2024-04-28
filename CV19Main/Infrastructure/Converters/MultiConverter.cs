bb2cb1d3fd4c721d4014e6acd0983978671766e2
MFlEGENDANTON <jffgx@mail.ru>
2024-04-28 13:38:05 +0300

    Добавлена реализация MultiConverter

---
+++ CV19Main/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CV19Main.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+     {
+         public abstract object Convert(object[] v, Type t, object p, CultureInfo c);
+
+        public virtual object[] ConvertBack(object v, Type[] t, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("Не поддерживается обратное преобразование");
+        }
+    }
+}
