a0c70522ea74a76c9cce42aae805e4ada119e6a6
MFlEGENDANTON <jffgx@mail.ru>
2024-04-29 19:51:58 +0300

    Инфраструктура для создания Хоста приложения

---
+++ CV19Main/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19Main
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true,
+                    reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+            return host_builder;
+        }
+    }
+}
