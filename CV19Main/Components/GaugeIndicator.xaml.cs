5fbbe91bcdd3724612afac08cdebbc4c8107841f
MFlEGENDANTON <jffgx@mail.ru>
2024-04-30 17:41:15 +0300

    Заготовка визуального элемента

---
+++ CV19Main/Components/GaugeIndicator.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CV19Main.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для GaugeIndicator.xaml
+    /// </summary>
+    public partial class GaugeIndicator : UserControl
+    {
+        public GaugeIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
