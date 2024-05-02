f85df1e4137ae933d8b8f5cdf1e16252cdf5f66c
MFlEGENDANTON <jffgx@mail.ru>
2024-05-02 19:55:46 +0300

    Тестирование работы Web приложения

---
+++ Tests/ClassLibrary1/WebServerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal static class WebServerTest
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
