163c50c739cf89e13cc1338ca58b8b59b11befff
MFlEGENDANTON <jffgx@mail.ru>
2024-05-02 20:41:53 +0300

    Простейшая логика реализации web-servera

--- CV19Main/Services/HttpListenerWebServer.cs
+++ CV19Main/Services/HttpListenerWebServer.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CV19.Web;
 using CV19Main.Services.Interfaces;
 
 namespace CV19Main.Services
 {
     internal class HttpListenerWebServer : IWebServiceServer
     {
-        public bool Enabled { get; set; }
-        public void Start()
+
+        private readonly WebServer _server = new WebServer(8080);
+
+        public bool Enabled { get => _server.Enabled; set => _server.Enabled = value; }
+        public void Start() => _server.Start();
+
+        public void Stop() => _server.Stop();
+
+        public HttpListenerWebServer()
         {
-            throw new NotImplementedException();
+            _server.RequestReceiver += OnRequestReceived;
         }
 
-        public void Stop()
+        private static void OnRequestReceived(object? sender, RequestReceiverEventArgs e)
         {
-            throw new NotImplementedException();
+            using var writer = new StreamWriter(e.Context.Response.OutputStream);
+
+            writer.WriteLine("CV-19 Application");
         }
     }
 }
