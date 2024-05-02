afce4f2202398ccbd156f68231161801636c4175
MFlEGENDANTON <jffgx@mail.ru>
2024-05-02 20:21:34 +0300

    Настройка логики работы веб сервиса

---
+++ CV19Main/ViewModels/WebServerViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Input;
+using CV19Main.Infrastructure.Commands;
+using CV19Main.ViewModels.Base;
+
+namespace CV19Main.ViewModels
+{
+    internal class WebServerViewModel : ViewModel
+    {
+
+        #region Enabled
+
+        private bool _Enabled;
+
+        public bool Enabled
+        {
+            get => _Enabled;
+            set => SetField(ref _Enabled, value);
+        }
+
+        #endregion
+
+        #region Command StartCommand - summary
+
+        ///<summary> summary </summary>
+
+        private ICommand _StartCommand;
+
+        public ICommand StartCommand => _StartCommand ??= new LambdaCommand(OnStartCommandExecuted, CanStartCommandExecute);
+
+        ///<summary>Проверка возможности выполнения - summary </summary>
+
+        private bool CanStartCommandExecute(object p) => !Enabled;
+
+        ///<summary>Логика выполнения - summary </summary>
+
+        private void OnStartCommandExecuted(object p)
+        {
+            Enabled = true;
+        }
+
+        #endregion
+
+        #region Command StopCommand - Stop
+
+
+        ///<summary> Stop </summary>
+
+        private ICommand _StopCommand;
+
+        ///<summary> Stop </summary>
+
+        public ICommand StopCommand => _StopCommand ??= new LambdaCommand(OnStopCommandExecuted, CanStopCommandExecute);
+
+        ///<summary>Проверка возможности выполнения - Stop </summary>
+
+        private bool CanStopCommandExecute(object p) => Enabled;
+
+        ///<summary>Логика выполнения - Stop </summary>
+
+        private void OnStopCommandExecuted(object p)
+        {
+            Enabled = false;
+        }
+
+        #endregion
+
+    }
+}
