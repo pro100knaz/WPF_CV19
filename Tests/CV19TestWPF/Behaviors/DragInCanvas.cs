5a92c74037c29e2eab1b113b7a55965f01fd60dc
MFlEGENDANTON <jffgx@mail.ru>
2024-05-03 12:57:45 +0300

    Поведение, способное реагировать на движение мыши

---
+++ Tests/CV19TestWPF/Behaviors/DragInCanvas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19TestWPF.Behaviours
+{
+    public class DragInCanvas : Behavior<UIElement>
+    {
+        private Point _StartPoint;
+
+        private Canvas canva;
+        protected override void OnAttached() //Поведение добавляется в коллекцию // и происходит нпример подписка или отписка от собыытий
+        { 
+            //ТИП ИЗ ПАРАМЕТРА ШАБЛОНА
+            AssociatedObject.MouseLeftButtonDown += OnLeftButtonDown;
+
+        }
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= OnLeftButtonDown;
+
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+
+        }
+
+        private void OnLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           if((canva ??= VisualTreeHelper.GetParent(AssociatedObject) as Canvas) is null) return;
+
+            _StartPoint = e.GetPosition(AssociatedObject);
+            AssociatedObject.CaptureMouse();
+            AssociatedObject.MouseMove += OnMouseMove;
+            AssociatedObject.MouseUp += OnMouseUp;
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+            AssociatedObject.ReleaseMouseCapture();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+
+            var obj = AssociatedObject;
+            var current_pos = e.GetPosition(canva);
+
+            var delta = current_pos - _StartPoint;
+
+            obj.SetValue(Canvas.LeftProperty, delta.X);
+            obj.SetValue(Canvas.TopProperty, delta.Y);
+
+        }
+    }
+}
