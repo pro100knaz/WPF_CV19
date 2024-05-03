ed4790db2c565233ea31c1b8ace873dc17080cb4
MFlEGENDANTON <jffgx@mail.ru>
2024-05-03 19:00:32 +0300

    Добавлены поведения для кнопок в панели

---
+++ CV19Main/Infrastructure/Behaviors/MinimizeWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19Main.Infrastructure.Behaviors;
+
+class MinimizeWindow : Behavior<Button>
+{
+    protected override void OnAttached()
+    {
+        AssociatedObject.Click += OnButtonClick;
+    }
+    protected override void OnDetaching()
+    {
+        AssociatedObject.Click -= OnButtonClick;
+    }
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
+    {
+        var window = AssociatedObject.FindVisualRoot() as Window;
+
+        if (window is null) return;
+
+        window.WindowState = WindowState.Minimized;
+           
+    }
+}
