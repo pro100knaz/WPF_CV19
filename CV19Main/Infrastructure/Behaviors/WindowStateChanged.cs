060e6b6be7d7266747371c89ab45fb64847e1e17
MFlEGENDANTON <jffgx@mail.ru>
2024-05-03 18:45:12 +0300

    Методы расширения для DependencyObject , позволяющие находить VISUAL AND Logical root

---
+++ CV19Main/Infrastructure/Behaviors/WindowStateChanged.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19Main.Infrastructure.Behaviors
+{
+    class WindowStateChanged : Behavior<Button>
+    {
+
+    }
+}
