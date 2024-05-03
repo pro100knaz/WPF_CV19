6a34af9a8787c40cdf140f95e70d2ba9c28e0f7b
MFlEGENDANTON <jffgx@mail.ru>
2024-05-03 18:45:12 +0300

    Методы расширения для DependencyObject , позволяющие находить VISUAL AND Logical root

---
+++ CV19Main/Infrastructure/Behaviors/CloseWindowBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19Main.Infrastructure.Behaviors
+{
+    public class CloseWindowBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.Click += OnButtonClick;
+        }
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= OnButtonClick;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = AssociatedObject;
+
+           var window = FindWindow(button) as Window;
+
+           window?.Close();
+
+
+        }
+        private static DependencyObject FindWindow(DependencyObject obj)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+
+                if (parent is null) return obj;
+                obj = parent;
+
+            } while (true);
+
+
+        }
+
+    }
+}
---
+++ CV19Main/Infrastructure/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace System.Window
+{
+    static class DependencyObjectExtensions
+    {
+        public static DependencyObject FindVisualRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+
+                if (parent is null) return obj;
+                obj = parent;
+
+            } while (true);
+        }
+        public static DependencyObject FindLogicalRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = LogicalTreeHelper.GetParent(obj);
+
+                if (parent is null) return obj;
+                obj = parent;
+
+            } while (true);
+        }
+
+    }
+}
