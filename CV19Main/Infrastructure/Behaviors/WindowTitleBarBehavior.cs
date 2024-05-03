babca297849410bba914d09efc6057b9c4ff7f11
MFlEGENDANTON <jffgx@mail.ru>
2024-05-03 19:12:28 +0300

    Добавил ещё расширения для DependencyObject

---
+++ CV19Main/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19Main.Infrastructure.Behaviors
+{
+    class WindowTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _Window;
+
+
+        protected override void OnAttached()
+        {
+            _Window = AssociatedObject as Window ?? AssociatedObject.Find
+        }
+
+        protected override void OnDetaching()
+        {
+          
+        }
+    }
+}
--- CV19Main/Infrastructure/Extensions/DependencyObjectExtensions.cs
+++ CV19Main/Infrastructure/Extensions/DependencyObjectExtensions.cs
@@ -33,5 +33,31 @@ namespace System.Windows
             } while (true);
         }
 
+
+        public static T FindVisualParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+            do
+            {
+                target = VisualTreeHelper.GetParent(target);
+            } while (target != null || target is T);
+
+            return target as T;
+        }
+
+        public static T FindLogicalParent<T>(this DependencyObject obj) where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+            do
+            {
+                target = LogicalTreeHelper.GetParent(target);
+            } while (target != null || target is T);
+
+            return target as T;
+        }
+
+
     }
 }
