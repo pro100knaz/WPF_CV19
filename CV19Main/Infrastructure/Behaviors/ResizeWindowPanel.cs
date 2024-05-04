adfa01746be2b2cd88533f08b548937d5572a815
MFlEGENDANTON <jffgx@mail.ru>
2024-05-04 21:10:49 +0300

    Добавлено изменение размеров окна

--- CV19Main/Infrastructure/Behaviors/ResizeWindowPanel.cs
+++ CV19Main/Infrastructure/Behaviors/ResizeWindowPanel.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Shapes;
+using CV19Main.Infrastructure.Extensions;
 using Microsoft.Xaml.Behaviors;
 
 namespace CV19Main.Infrastructure.Behaviors
@@ -43,32 +44,61 @@ namespace CV19Main.Infrastructure.Behaviors
             }
 
         }
-
-        private void ResizeLine(Line line , Window window)
+        public enum SizingAction
+        {
+            West = 1,
+            East = 2,
+            North = 3,
+            NorthWest = 4,
+            NorthEast = 5,
+            South = 6,
+            SouthWest = 7,
+            SouthEast = 8,
+        }
+        private static void ResizeLine(Line line, Window window)
         {
             switch (line.VerticalAlignment)
             {
                 case VerticalAlignment.Top:
-
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.North), IntPtr.Zero);
                     break;
 
                 case VerticalAlignment.Bottom:
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.South), IntPtr.Zero);
                     break;
 
                 default:
                     switch (line.HorizontalAlignment)
                     {
                         case HorizontalAlignment.Left:
+                            window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.West), IntPtr.Zero);
                             break;
+
                         case HorizontalAlignment.Right:
+                            window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.East), IntPtr.Zero);
                             break;
                     }
                     break;
             }
-        } 
-        private void ResizeRect(Rectangle rect, Window window)
-        {
+        }
 
+        private static void ResizeRect(Rectangle rect, Window window)
+        {
+            switch (rect.VerticalAlignment)
+            {
+                case VerticalAlignment.Top when rect.HorizontalAlignment == HorizontalAlignment.Left:
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.NorthWest), IntPtr.Zero);
+                    break;
+                case VerticalAlignment.Top when rect.HorizontalAlignment == HorizontalAlignment.Right:
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.NorthEast), IntPtr.Zero);
+                    break;
+                case VerticalAlignment.Bottom when rect.HorizontalAlignment == HorizontalAlignment.Left:
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.SouthWest), IntPtr.Zero);
+                    break;
+                case VerticalAlignment.Bottom when rect.HorizontalAlignment == HorizontalAlignment.Right:
+                    window.SendMessage(WM.SYSCOMMAND, (IntPtr)((int)SC.SIZE + SizingAction.SouthEast), IntPtr.Zero);
+                    break;
+            }
         }
     }
 }
