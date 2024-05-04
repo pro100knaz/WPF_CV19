7635d55820864e68d4066c3eb65167ee7c1b5c05
MFlEGENDANTON <jffgx@mail.ru>
2024-05-04 09:30:08 +0300

    Макет представления окна управления студентами

---
+++ CV19Main/ViewModels/StudentsManagementViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19Main.ViewModels.Base;
+
+namespace CV19Main.ViewModels
+{
+    internal class StudentsManagementViewModel : ViewModel
+    {
+
+
+        #region string Title - "Заголовок Окна"
+
+        ///<summary> Заголовок Окна </summary>
+        private string _Title = "Student Management";
+        ///<summary> Заголовок Окна </summary>
+        public string Title
+        {
+            get => _Title;
+            set => SetField(ref _Title, value);
+        }
+
+        #endregion
+
+        public StudentsManagementViewModel()
+        {
+            
+
+        }
+    }
+}
